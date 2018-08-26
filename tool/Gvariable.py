4274255de4bd343cebb2d78d94dace241f14e4b1
reynold2 <www.47812005@qq.com>
2018-08-26 14:04:26 +0800

    【配置完成，测试可执行，两边保持一致了】

--- tool/Gvariable.py
+++ tool/Gvariable.py
@@ -1,1 +1,1 @@
-PATHDATA={}
+PATHDATA={'case':"D:\work\testtool\tool\res\TestData.xls",'data':"D:\work\testtool\tool\res",'exe':"D:\work\testtool\tool\res\QxPlugin\app.exe",'report':"D:\work\testtool\tool\resreport.xls",'config':"D:/work/testtool/tool/res/config.ini"}
